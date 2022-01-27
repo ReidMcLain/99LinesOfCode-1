b389f039608af7e5cfc076bf476da3386a4a20e3
ReidMcLain <reidmm@gmail.com>
2022-01-27 13:12:33 -0600

    some changes

--- app.js
+++ app.js
@@ -3,14 +3,13 @@ let myFriends = ['Sage', 'William', 'John', 'Noah', 'Chad']
 for (let i = 0; i < myFriends.length; i++) {
 let friend = myFriends[i];  
 console.log(myFriends[i].toUpperCase() + ":");
-
-    for(let i = 99; i > 0; i--) {
-        if(i > 2) { 
-            console.log(i + " lines of code in the file, " + i + " lines of code; " + friend + " strikes one out, clears it all out, " + (i - 1) + " lines of code in the file.");
-        } else if(i == 2) { 
-            console.log(i + " lines of code in the file, " + i + " lines of code; " + friend + " strikes one out, clears it all out, " + (i - 1 ) + " line of code in the file.");
-        } else if (i == 1) { 
-            console.log(i + " line of code in the file, " + i + " line of code; " + friend + " strikes one out, clears it all out, no more lines of code in the file!");
+    for(let j = 99; j > 0; j--) {
+        if(j > 2) { 
+            console.log(j + " lines of code in the file, " + j + " lines of code; " + friend + " strikes one out, clears it all out, " + (j - 1) + " lines of code in the file.");
+        } else if(j == 2) { 
+            console.log(j + " lines of code in the file, " + j + " lines of code; " + friend + " strikes one out, clears it all out, " + (j - 1 ) + " line of code in the file.");
+        } else if (j == 1) { 
+            console.log(j + " line of code in the file, " + j + " line of code; " + friend + " strikes one out, clears it all out, no more lines of code in the file!");
         };
     };
 };  
