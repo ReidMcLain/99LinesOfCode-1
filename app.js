60eda7222ffab5a289ec8acb72da1b0004b3a81b
ReidMcLain <reidmm@gmail.com>
2022-01-27 15:32:29 -0600

    small change

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ console.log(myFriends[i].toUpperCase() + ":");
         if(j > 2) { 
             console.log(j + " lines of code in the file, " + j + " lines of code; " + friend + " strikes one out, clears it all out, " + (j - 1) + " lines of code in the file.");
         } else if(j == 2) { 
-            console.log(j + " lines of code in the file, " + j + " lines of code; " + friend + " strikes one out, clears it all out, " + (j - 1 ) + " line of code in the file.");
+            console.log(j + " lines of code in the file, " + j + " lines of code; " + friend + " strikes one out, clears it all out, " + (j - 1) + " line of code in the file.");
         } else if (j == 1) { 
             console.log(j + " line of code in the file, " + j + " line of code; " + friend + " strikes one out, clears it all out, no more lines of code in the file!");
         };
